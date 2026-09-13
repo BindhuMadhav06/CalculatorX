9c0f7d4e064ff7f5f609da6cb95e70a4b3e3ea9c
Bindhu Madhav Gorajana <bindhumadhav2006@gmail.com>
2026-09-13 23:20:12 +0530

    Add main entry point to package.json and update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-// Alias asset-registry to react-native/Libraries/Image/AssetRegistry for Metro image asset loader
 config.resolver = config.resolver || {};
 config.resolver.extraNodeModules = {
-  ...config.resolver.extraNodeModules,
+  ...(config.resolver.extraNodeModules || {}),
   'react-native/asset-registry': require.resolve('react-native/Libraries/Image/AssetRegistry'),
 };
 
