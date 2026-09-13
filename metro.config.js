d74065c912ffe8b480e311a1fc9c33d1b18233f1
Bindhu Madhav Gorajana <bindhumadhav2006@gmail.com>
2026-09-14 01:02:28 +0530

    Add explicit sdkVersion 50.0.0 to app.json for clean Expo cloud build

--- metro.config.js
+++ metro.config.js
@@ -2,10 +2,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver = config.resolver || {};
-config.resolver.extraNodeModules = {
-  ...(config.resolver.extraNodeModules || {}),
-  'react-native/asset-registry': require.resolve('react-native/Libraries/Image/AssetRegistry'),
-};
-
 module.exports = config;
