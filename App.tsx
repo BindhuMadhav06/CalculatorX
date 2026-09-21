0a99585ff9053176746848c21545d748d5e47390
Bindhu Madhav Gorajana <bindhumadhav2006@gmail.com>
2026-09-21 21:53:54 +0530

    Replace react-native-get-random-values with native expo-crypto polyfill to eliminate top-level native module launch crash

--- App.tsx
+++ App.tsx
@@ -1,8 +1,7 @@
-import 'react-native-get-random-values';
-import React, { Component, ReactNode, useEffect } from 'react';
+import './src/utils/cryptoPolyfill';
+import React, { Component, ReactNode } from 'react';
 import { StatusBar, StyleSheet, View, Text } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
-import * as SplashScreen from 'expo-splash-screen';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 interface ErrorBoundaryProps {
@@ -41,18 +40,6 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
 }
 
 export default function App() {
-  useEffect(() => {
-    async function prepare() {
-      try {
-        await SplashScreen.preventAutoHideAsync();
-      } catch (e) {}
-      try {
-        await SplashScreen.hideAsync();
-      } catch (e) {}
-    }
-    prepare();
-  }, []);
-
   return (
     <SafeAreaProvider style={styles.container}>
       <ErrorBoundary>
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import 'react-native-get-random-values';
+import './src/utils/cryptoPolyfill';
 import { registerRootComponent } from 'expo';
 import App from './App';
 
