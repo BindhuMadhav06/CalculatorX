0c6aa20d486f28c928398ee9e479313e57491e8c
Bindhu Madhav Gorajana <bindhumadhav2006@gmail.com>
2026-09-22 21:13:20 +0530

    Fix syntax in index.js for plain JS compatibility

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ import { registerRootComponent } from 'expo';
 import App from './App';
 
 // Guaranteed global.crypto.getRandomValues polyfill
-if (typeof global.crypto !== 'object') {
-  global.crypto = {} as any;
+if (typeof global.crypto !== 'object' || global.crypto === null) {
+  global.crypto = {};
 }
 if (typeof global.crypto.getRandomValues !== 'function') {
-  global.crypto.getRandomValues = function getRandomValues(array: any) {
+  global.crypto.getRandomValues = function getRandomValues(array) {
     const getRandomBytes = Crypto.getRandomBytes(array.length);
     for (let i = 0; i < array.length; i++) {
       array[i] = getRandomBytes[i];
